0830b573379c3cfcb79355d256bc35f93e2407af
jerry <jerryren.5279@gmail.com>
2026-07-30 21:32:33 -0400

    Add privacy policy page

--- itemory_project/urls.py
+++ itemory_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -29,6 +30,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path('admin/', admin.site.urls),
     path('app/', include("app.urls")),
+    path('privacy/', TemplateView.as_view(template_name="privacy_policy.html"), name="privacy-policy"),
 
     # Simplejwt Library
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
